aff8cda934047d9f9911959094fb2c5567885115
Rishi0cybertech <gauttamrishi215@gmail.com>
2026-03-08 15:59:00 +0530

    Initialize WiFi and temperature sensor in main.ino
    Added WiFi connection setup and temperature sensor initialization.

--- Output_Monitering/main.ino
+++ Output_Monitering/main.ino
@@ -1,1 +1,43 @@
+#include <WiFi.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
+#define BUTTON 12
+#define PRESSURE 34
+#define BUZZER 27
+#define TEMP_PIN 4
+
+const char* ssid = "Wifi-Name";
+const char* password = "Password";
+
+OneWire oneWire(TEMP_PIN);
+DallasTemperature sensors(&oneWire);
+
+void connectWiFi()
+{
+    WiFi.begin(ssid, password);
+
+    Serial.print("Connecting to WiFi");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println();
+    Serial.println("WiFi Connected");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+}
+void setup()
+{
+    Serial.begin(115200);
+    pinMode(BUTTON, INPUT_PULLUP);
+    pinMode(PRESSURE, INPUT);
+    pinMode(BUZZER, OUTPUT);
+    sensors.begin();
+    connectWiFi();
+    Serial.println("ESP32 is working");
+}
+void loop()
+{
+}
