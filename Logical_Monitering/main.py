5fff5c7c904b8530f262a5c4b05b2ac2729ac3fa
Priyanshu Raj <pr590315@gmail.com>
2026-03-09 13:52:30 +0530

    Added the warning conditions
    This code can also check the conditions of and the warning, to inform the hospital as soon as possible

--- Logical_Monitering/main.py
+++ Logical_Monitering/main.py
@@ -6,6 +6,7 @@ import time
 while True:
     # adding the API for making connection between the ESP32 and the server
     url1 = "XXX" # <------- Add the ULR of the API here
+    url2 = "XXX" # <------- Add the ULR of the API here
     # x is a verible defined for the request sent to the API server
     x = requests.get(url1)
     # printing the status code of the API
@@ -14,5 +15,27 @@ while True:
     y = x.json()
     #printing the response received from the API server
     print(y)
+    #creating a for loop to iterate through the response received from the API server, because the response is in the form {"id": {"message": "status"}}
+    for firebase_id, inner_dict in y.items():
+        # This will print the status of the patient
+        print(inner_dict['message'])
+        # this condition will check the status of the patient and fid out that if the patient is safe, warning or danger
+        if 1 <= int(inner_dict['message']) <= 3:
+            print("safe")
+            # This condition is to define that the condition is safe
+            payload = {"message": "safe"}
+            requests.post(url2, json=payload)
+        elif 3 < int(inner_dict['message']) <= 5:
+            print("warning!")
+            # This condition is to define that the condition is warning
+            payload = {"message": "warning"}
+            requests.post(url2, json=payload)
+        else:
+            print("danger! API lost")
+            # This condition is to define that the condition is danger
+            payload = {"message": "API_lost"}
+            requests.post(url2, json=payload)
+    # This will delete the data from the API server so it recheck the status of the patient from the old data
+    requests.delete(url1)
     # adding a sleep time of 1 second so it doesn't spam the server
     time.sleep(1)
