ec2488b7ab52523aad7145d1f31df91fb61ee2fa
Priyanshu Raj <pr590315@gmail.com>
2026-03-09 12:06:30 +0530

    Adding the comments
    adding comments to make the code understandable for others and more defined

--- Logical_Monitering/main.py
+++ Logical_Monitering/main.py
@@ -1,11 +1,18 @@
 #/home/priyanshu/Documents/GitHub/Hospital-Management-System/Logical_Monitering
+#importing libraries
 import requests
 import time
-
+# opening the loop because we want the status of the patient
 while True:
-    url1 = "XXX"
+    # adding the API for making connection between the ESP32 and the server
+    url1 = "XXX" # <------- Add the ULR of the API here
+    # x is a verible defined for the request sent to the API server
     x = requests.get(url1)
+    # printing the status code of the API
     print(f"STATUS CODE: {x.status_code}")
+    # y is a verible defined for the response received from the API server in the json format
     y = x.json()
+    #printing the response received from the API server
     print(y)
+    # adding a sleep time of 1 second so it doesn't spam the server
     time.sleep(1)
