c6c060ddaca49907b2bea7ffe7325bb4f6e7d9e1
Priyanshu Raj <pr590315@gmail.com>
2026-03-14 15:19:19 +0530

    Fixed few issue and errors

--- Logical_Monitering/main.py
+++ Logical_Monitering/main.py
@@ -15,26 +15,39 @@ while True:
     y = x.json()
     #printing the response received from the API server
     print(y)
-    #creating a for loop to iterate through the response received from the API server, because the response is in the form {"id": {"message": "status"}}
-    for firebase_id, inner_dict in y.items():
-        # This will print the status of the patient
-        print(inner_dict['message'])
-        # this condition will check the status of the patient and fid out that if the patient is safe, warning or danger
-        if 1 <= int(inner_dict['message']) <= 3:
-            print("safe")
-            # This condition is to define that the condition is safe
-            payload = {"message": "safe"}
-            requests.post(url2, json=payload)
-        elif 3 < int(inner_dict['message']) <= 5:
-            print("warning!")
-            # This condition is to define that the condition is warning
-            payload = {"message": "warning"}
-            requests.post(url2, json=payload)
-        else:
-            print("danger! API lost")
-            # This condition is to define that the condition is danger
-            payload = {"message": "API_lost"}
-            requests.post(url2, json=payload)
+    if type(y) == dict:
+        # creating a for loop to iterate through the response received from the API server, because the response is in the form {"id": {"message": "status"}}
+        for firebase_id, inner_dict in y.items():
+            # This will print the status of the patient
+            print(inner_dict['message'])
+            # This condition will check that whether if there is any integer
+            if int(inner_dict['message']):
+                # this condition will check the status of the patient and fid out that if the patient is safe, warning or danger
+                if 1 <= int(inner_dict['message']) <= 3:
+                    print("safe")
+                    # This condition is to define that the condition is safe
+                    payload = {"message": "safe"}
+                    requests.post(url2, json=payload)
+                elif 3 < int(inner_dict['message']) <= 5:
+                    print("warning!")
+                    # This condition is to define that the condition is warning
+                    payload = {"message": "warning"}
+                    requests.post(url2, json=payload)
+                else:
+                    print("danger! invalid input")
+                    # This condition is to define that the condition is danger
+                    payload = {"message": "invalid input"}
+                    requests.post(url2, json=payload)
+            else:
+                print("invalid input")
+                # This condition is to define that the condition is danger
+                payload = {"message": "invalid input"}
+                requests.post(url2, json=payload)
+    else:
+        print("danger! API lost")
+        # This condition is to define that the condition is danger
+        payload = {"message": "API_lost"}
+        requests.post(url2, json=payload)
     # This will delete the data from the API server so it recheck the status of the patient from the old data
     requests.delete(url1)
     # adding a sleep time of 1 second so it doesn't spam the server
